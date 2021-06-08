e322cb4e69a270ba448dceb8fcc3d7e64942ba23
sokainadaabal <sokainadaabal@gmail.com>
2021-06-08 12:23:07 +0100

    hello

--- src/main/java/com/ProductionScientifique/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ProductionScientifique.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
